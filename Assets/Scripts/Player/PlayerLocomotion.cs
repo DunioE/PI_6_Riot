78cd309f1be2d02153bca6b46fd53bb4b8f982c2
DunioE <126677297+DunioE@users.noreply.github.com>
2024-01-02 10:52:20 +0000

    5. Animação e Camera
    Continuei a implementação das animações e corrigi possíveis erros relacionados a elas, também iniciei o controlo da câmera do jogador.

--- Assets/Scripts/Player/PlayerLocomotion.cs
+++ Assets/Scripts/Player/PlayerLocomotion.cs
@@ -51,6 +51,8 @@ namespace SG
             Vector3 projectedVelocity = Vector3.ProjectOnPlane(moveDirection, normalVector);
             rigidbody.velocity = projectedVelocity;
 
+            animatorHandler.UpdateAnimatorValues(inputHandler.moveAmount, 0);
+
             if (animatorHandler.canRotate)
             {
                 HandleRotation(delta);
